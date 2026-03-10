73487057fd083932cf61511c4dc590105edd3c85
Shaik Younis <sy3936@srmist.edu.in>
2026-03-10 23:58:54 +0530

    JAVA WEEK PRACTISE

---
+++ Level3_Programs.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Level3_Programs {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter year: ");
+        int year = input.nextInt();
+
+        // Multiple If
+        if (year >= 1582) {
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    if (year % 400 == 0)
+                        System.out.println("Leap Year");
+                    else
+                        System.out.println("Not Leap Year");
+                } else
+                    System.out.println("Leap Year");
+            } else
+                System.out.println("Not Leap Year");
+        }
+
+        // Single If
+        if (year >= 1582 &&
+            (year % 4 == 0 &&
+            (year % 100 != 0 || year % 400 == 0)))
+            System.out.println("Leap Year (Single If)");
+
+        input.close();
+    }
+}
---
+++ Week.java
@@ -0,0 +1,6 @@
+public class Week {
+    public static void main(String[] args) {
+        System.out.println("Java Week Practice Working!");
+    }
+}
+ 
