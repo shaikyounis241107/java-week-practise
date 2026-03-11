96d0f483a4f8ccbb43706fa258b40f063f094857
Shaik Younis <sy3936@srmist.edu.in>
2026-03-11 09:29:54 +0530

    Write a program to create a basic calculator for addition, subtraction, multiplication, and division. The program should ask for two numbers (floating point) and perform all the operations

---
+++ WeeklyPracticeLevelTwo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class WeeklyPracticeLevelTwo {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        float number1 = input.nextFloat();
+
+        System.out.print("Enter second number: ");
+        float number2 = input.nextFloat();
+
+        float addition = number1 + number2;
+        float subtraction = number1 - number2;
+        float multiplication = number1 * number2;
+        float division = number1 / number2;
+
+        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
+                + number1 + " and " + number2 + " is "
+                + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
+    }
+}
