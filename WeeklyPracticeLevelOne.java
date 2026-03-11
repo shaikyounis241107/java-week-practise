5879ca880b4b32f151c03f199fd4a2dfee352b2e
Shaik Younis <sy3936@srmist.edu.in>
2026-03-11 09:20:05 +0530

    Sam’s mark in Maths is 94, Physics is 95 and Chemistry is 96 out of 100. Find the average percent mark in PCM

--- WeeklyPracticeLevelOne.java
+++ WeeklyPracticeLevelOne.java
@@ -1,11 +1,12 @@
 public class WeeklyPracticeLevelOne {
     public static void main(String[] args) {
 
-        int birthYear = 2000;
-        int currentYear = 2024;
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
 
-        int age = currentYear - birthYear;
+        double average = (maths + physics + chemistry) / 3.0;
 
-        System.out.println("Harry's age in 2024 is " + age);
+        System.out.println("Sam’s average mark in PCM is " + average);
     }
 }
