88ddabbedf00754f507bb8a6ddeeb0034fb348de
Shaik Younis <sy3936@srmist.edu.in>
2026-03-11 09:31:31 +0530

    Write a program that takes the base and height in cm to find the area of a triangle in square inches and square centimeters

--- WeeklyPracticeLevelTwo.java
+++ WeeklyPracticeLevelTwo.java
@@ -5,19 +5,15 @@ public class WeeklyPracticeLevelTwo {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter first number: ");
-        float number1 = input.nextFloat();
+        System.out.print("Enter base in cm: ");
+        double base = input.nextDouble();
 
-        System.out.print("Enter second number: ");
-        float number2 = input.nextFloat();
+        System.out.print("Enter height in cm: ");
+        double height = input.nextDouble();
 
-        float addition = number1 + number2;
-        float subtraction = number1 - number2;
-        float multiplication = number1 * number2;
-        float division = number1 / number2;
+        double areaCm = 0.5 * base * height;
+        double areaInch = areaCm / (2.54 * 2.54);
 
-        System.out.println("The addition, subtraction, multiplication, and division value of 2 numbers "
-                + number1 + " and " + number2 + " is "
-                + addition + ", " + subtraction + ", " + multiplication + ", and " + division);
+        System.out.println("The Area of the triangle in sq in is " + areaInch + " and sq cm is " + areaCm);
     }
 }
