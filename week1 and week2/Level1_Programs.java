44ccaa1b455396239f9c8aefa3b751772fb2d354
Shaik Younis <sy3936@srmist.edu.in>
2026-04-13 09:27:47 +0530

    Added week 3 and week 4 programs

---
+++ week1 and week2/Level1_Programs.java
@@ -0,0 +1,23 @@
+public class Level1_Programs {
+
+    public static void main(String[] args) {
+
+        // 1 Age
+        String name = "Harry";
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+        System.out.println(name + "'s age is " + age);
+
+        // 2 Average PCM
+        int maths = 94, physics = 95, chemistry = 96;
+        double average = (maths + physics + chemistry) / 3.0;
+        System.out.println("Average PCM mark is " + average);
+
+        // 3 KM to Miles
+        double km = 10.8;
+        double miles = km / 1.6;
+        System.out.println("Distance in miles: " + miles);
+    }
+}
+ ⁠
